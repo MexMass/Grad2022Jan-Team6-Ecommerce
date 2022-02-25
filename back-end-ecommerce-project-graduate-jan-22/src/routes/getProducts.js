1c1d3a608863170fea1b2e2a6d5cc737b71bc4e5
Ricky Cheng <rickycheng@hotmail.co.uk>
2022-02-25 14:55:59 +0000

    Completed fetching products by tag

--- back-end-ecommerce-project-graduate-jan-22/src/routes/getProducts.js
+++ back-end-ecommerce-project-graduate-jan-22/src/routes/getProducts.js
@@ -10,12 +10,12 @@ router.get("/tag/:tag", getProductByTag);
 
 function getProducts(req, res) {
   pool.query(
-    "SELECT * FROM products WHERE discontinued = 'false'",
+    "SELECT * FROM products WHERE discontinued = false",
     (error, result) => {
       if (error) {
         return res.status(500).send("Internal Error");
       } else {
-        return res.send(result.rows);
+        return res.json(result.rows);
       }
     }
   );
@@ -24,30 +24,30 @@ function getProducts(req, res) {
 function getProductById(req, res) {
   const id = parseInt(req.params.id);
   pool.query(
-    `SELECT * FROM products WHERE id=${id} AND discontinued = 'false' `,
+    `SELECT * FROM products WHERE id=${id} AND discontinued = false `,
     (error, result) => {
       if (error) {
         return res.status(500).send("internal Error");
       } else {
-        return res.send(result.rows);
+        return res.json(result.rows);
       }
     }
   );
 }
 
 function getProductByTag(req, res) {
-  const tag = parseInt(req.params.tag);
+  const tag = req.params.tag;
   pool.query(
     `SELECT DISTINCT(products.id), products.name, units_in_stock, total_price, image_url
-    FROM products_tags
+    FROM product_tags
     INNER JOIN products on (product_tags.product_id = products.id)
     INNER JOIN tags on (product_tags.tag_id = tags.id)
-    WHERE tag.name ='${tag}' AND products.discontinued ='false'`,
+    WHERE tags.name ='${tag}' AND products.discontinued =false`,
     (error, result) => {
       if (error) {
         return res.status(500).send("internal Error");
       } else {
-        return res.send(result.rows);
+        return res.json(result.rows);
       }
     }
   );
