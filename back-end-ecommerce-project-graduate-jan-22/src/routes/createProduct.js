bacda33029b9d5b3632a550c8ab67ec2b9529e91
Lukas Ramonas <ramonas.lukas@gmail.com>
2022-02-28 15:31:04 +0000

    added discontinueProduct function & endpoint

--- back-end-ecommerce-project-graduate-jan-22/src/routes/createProduct.js
+++ back-end-ecommerce-project-graduate-jan-22/src/routes/createProduct.js
@@ -2,9 +2,10 @@ const express = require("express");
 const pool = require("../dbconnection");
 const router = express.Router();
 
-router.post("/create", newProduct); // endpoint - http://localhost:3000/db/create
-router.post("/addDiscount", newDiscount); // endpoint - http://localhost:3000/db/addDiscount
-router.delete("/deleteDiscount/:id", deleteDiscount); // endpoint - http://localhost:3000/db/addDiscount
+router.post("/create", newProduct); // endpoint - http://localhost:3000/products/create
+router.post("/addDiscount", newDiscount); // endpoint - http://localhost:3000/products/addDiscount
+router.delete("/deleteDiscount/:id", deleteDiscount); // endpoint - http://localhost:3000/products/deleteDiscount/id
+router.put("/discontinue/:id", discontinueProduct); // endpoint - http://localhost:3000/products/discontinue/id
 
 function newProduct(req, res) {
   const { name, supplier_name, units_in_stock, total_price, discontinued, image_url } = req.body; // get details from req.body
@@ -25,6 +26,29 @@ function newProduct(req, res) {
 
     console.log('Data inserted into products table');
     res.json({response: "Product " + name + " created."});
+  });
+}
+
+function discontinueProduct(req, res) {
+  const id = parseInt(req.params.id); // get id from parameters
+
+  let myquery = // query for insert
+  `
+  UPDATE products
+  SET discontinued = true 
+  WHERE id = ${id};
+  `;
+
+  pool.query(myquery, (error, response) => {
+    
+    if (error) {
+        console.error(error);
+        res.json({response: error.message}); // if error then send response
+        return;
+    }
+
+    console.log('Data changed in products table');
+    res.json({response: "Product with id " + id + " discontinued."})
   });
 }
 
