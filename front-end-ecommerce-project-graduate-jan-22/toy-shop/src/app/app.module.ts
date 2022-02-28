21b3354f7173c8827c31e619cb274c15adb0818d
Ricky Cheng <rickycheng@hotmail.co.uk>
2022-02-28 16:30:12 +0000

    Created toy service, imported into toy component

--- front-end-ecommerce-project-graduate-jan-22/toy-shop/src/app/app.module.ts
+++ front-end-ecommerce-project-graduate-jan-22/toy-shop/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToyComponent } from './components/toy/toy.component';
+import { ToyService } from './services/toy.service';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,10 @@ import { ToyComponent } from './components/toy/toy.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ToyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
