1d88e9ea955ebbedeab90f3564e6858d29279cb2
Ricky Cheng <rickycheng@hotmail.co.uk>
2022-03-02 11:14:15 +0000

    Added navbar and basic routing for toys

--- front-end-ecommerce-project-graduate-jan-22/toy-shop/src/app/app.module.ts
+++ front-end-ecommerce-project-graduate-jan-22/toy-shop/src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule , Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToyComponent } from './components/toy/toy.component';
 import { ToyService } from './services/toy.service';
 import { FormsModule } from '@angular/forms';
+
+//http://localhost:4200/
+
+const routes: Routes  = [
+  //{path: 'home' , component: HomeComponent},
+  // {path : 'login' , component :LoginComponent },
+  {path : 'toys', component : ToyComponent},
+  //{path: 'menu/:id/name/:name', component: ProductDetailComponent},
+  //{path: '', redirectTo: '/home', pathMatch: 'full'},
+  //{path: '**', component: PagenotfoundComponent}
+];
 
 @NgModule({
   declarations: [
@@ -17,7 +29,8 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ToyService],
   bootstrap: [AppComponent]
