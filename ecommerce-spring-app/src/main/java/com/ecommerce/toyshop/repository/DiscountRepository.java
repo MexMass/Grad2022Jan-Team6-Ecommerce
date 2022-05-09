e784bb20829b515e68d4f1697449cb25c58caeb8
Ricky Cheng <rickycheng@hotmail.co.uk>
2022-05-09 02:06:58 +0100

    Fixed Swagger config

--- ecommerce-spring-app/src/main/java/com/ecommerce/toyshop/repository/DiscountRepository.java
+++ ecommerce-spring-app/src/main/java/com/ecommerce/toyshop/repository/DiscountRepository.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.ecommerce.toyshop.entities.Discount;
 
-public interface DiscountRepository extends JpaRepository {
+@Repository
+public interface DiscountRepository extends JpaRepository<Discount,Long> {
 	
 	@Query(value="SELECT percent FROM discounts WHERE product_id = :percent ",nativeQuery=true)
 	Optional<Discount> percentByProductId(@Param("percent")Long productId);
